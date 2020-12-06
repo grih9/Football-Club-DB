f547d83c9d1bcefd2e662ae8a9930e70f2f1b2ab
Tolstikov Grigoriy <tolstikov.gn@edu.spbstu.ru>
2020-12-06 03:31:36 +0300

    Добавлена авторизация, некоторые меню

--- fcdb/main.py
+++ fcdb/main.py
@@ -2,8 +2,14 @@ from PyQt5 import QtWidgets
 from startMenu import Ui_MainWindow as startmain
 from login import Ui_MainWindow as loginmain
 from registerFan import Ui_MainWindow as regFanmain
+from managerMenu import Ui_MainWindow as managerMenuMain
+from choose import Ui_MainWindow as managerChooseMain
+from cryptography.fernet import Fernet
 import sys
 import pyodbc
+
+current_role = 0
+current_userID = 0
 
 class mainwindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -37,7 +43,6 @@ class regFanWindow(QtWidgets.QMainWindow):
         super().__init__()
         self.ui = regFanmain()
         self.ui.setupUi(self)
-        self.ui.setupUi(self)
         self.setWindowTitle("Регистрация болельщика")
         self.ui.backButton.clicked.connect(self.backButton_clicked)
         self.ui.passwodCheckbox.stateChanged.connect(self.checkboxHandler)
@@ -59,10 +64,107 @@ class loginindow(QtWidgets.QMainWindow):
         self.ui = loginmain()
         self.ui.setupUi(self)
         self.setWindowTitle("Вход")
-        self.ui.enterButton_2.clicked.connect(self.backButton_clicked)
+        self.ui.enterButton.clicked.connect(self.enterButton_clicked)
+        self.ui.backButton.clicked.connect(self.backButton_clicked)
+    def enterButton_clicked(self):
+        l = self.ui.loginLine.text()
+        p = self.ui.passwordLine.text()
+        if (l.strip() == ''):
+            message = "Поле логина пустое! Введите логин!"
+            error_message = QtWidgets.QErrorMessage(self)
+            error_message.setWindowTitle("Ошибка входа")
+            error_message.showMessage(message)
+            if len(l) != 0:
+                self.ui.loginLine.clear()
+        elif (p.strip() == ''):
+            message = "Поле пароля пустое! Введите пароль!"
+            error_message = QtWidgets.QErrorMessage(self)
+            error_message.setWindowTitle("Ошибка входа")
+            error_message.showMessage(message)
+            if len(p) != 0:
+                self.ui.passwordLine.clear()
+        elif (l.find(' ') != -1):
+            message = "Недопустимый символ в поле логина. Проверьте правильность данных и повторите вход."
+            error_message = QtWidgets.QErrorMessage(self)
+            error_message.setWindowTitle("Ошибка входа")
+            error_message.showMessage(message)
+            self.ui.loginLine.clear()
+        elif (p.find(' ') != -1):
+            message = "Данного пользователя не существует или введен неверный пароль! Проверьте правильность данных и повторите вход."
+            error_message = QtWidgets.QErrorMessage(self)
+            error_message.setWindowTitle("Ошибка входа")
+            error_message.showMessage(message)
+            self.ui.passwordLine.clear()
+        else:
+            self.db =  Sql("football_club")
+            status, role, id = checkPassword(self.db, l, p)
+            self.db.cnxn.close()
+            if (status == False):
+                message = "Данного пользователя не существует или введен неверный пароль! Проверьте правильность данных и повторите вход."
+                error_message = QtWidgets.QErrorMessage(self)
+                error_message.setWindowTitle("Ошибка входа")
+                error_message.showMessage(message)
+                self.ui.passwordLine.clear()
+            else:
+                global current_role
+                global  current_userID
+                current_role = role
+                current_userID = id
+                if current_role == 1:
+                    self.menu = managerMenuWindow()
+                    self.menu.show()
+                    self.close()
+
     def backButton_clicked(self):
         self.main = mainwindow()
         self.main.show()
+        self.close()
+
+
+class managerMenuWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = managerMenuMain()
+        self.ui.setupUi(self)
+        self.setWindowTitle("Руководство")
+        self.ui.exitButton.clicked.connect(self.exitButton_clicked)
+        self.ui.managingButton.clicked.connect(self.managingButton_clicked)
+
+    def exitButton_clicked(self):
+        message = 'Вы уверены что хотите выйти?'
+        reply = QtWidgets.QMessageBox.question(self, 'Выход из базы данных', message,
+                                               QtWidgets.QMessageBox.Yes,
+                                               QtWidgets.QMessageBox.No)
+        if reply == QtWidgets.QMessageBox.Yes:
+            global current_role
+            global current_userID
+            current_role = 0
+            current_userID = 0
+            self.main = mainwindow()
+            self.main.show()
+            self.close()
+
+    def managingButton_clicked(self):
+        self.managingWindow = managerManagingWindow()
+        self.managingWindow.show()
+        self.close()
+
+class managerManagingWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = managerChooseMain()
+        self.ui.setupUi(self)
+        self.ui.coachButton.hide()
+        self.ui.playerButton.hide()
+        self.ui.fanButton.hide()
+        self.ui.stadiumButton.hide()
+        self.ui.teamButton.hide()
+        self.ui.managerButton.hide()
+        self.setWindowTitle("Руководство")
+        self.ui.backButton.clicked.connect(self.backButton_clicked)
+    def backButton_clicked(self):
+        self.menu = managerMenuWindow()
+        self.menu.show()
         self.close()
 
 class Sql:
@@ -72,13 +174,58 @@ class Sql:
                                    "Database="+database+";"
                                    "Trusted_Connection=yes;")
         self.cursor = self.cnxn.cursor()
+    def login(self, login, password):
+        self.cursor = self.cnxn.cursor()
+        self.cursor.execute("SELECT password")
+
+
+def checkPassword(db, login, password):
+    cipher_key = b'aIgO-OFHtCwB6LgfBcl1IQPYTVjQHzNsyzHK_PICN3s='
+    cipher = Fernet(cipher_key)
+    db.cursor.execute("SELECT ID_пользователя, Пароль, Роль FROM Пользователи where Логин='" + login + "'")
+    row = db.cursor.fetchone()
+    if row != None:
+        passw = cipher.decrypt(str.encode(row[1])).decode('utf8')
+        if passw == password:
+            return True, row[2], row[0]
+        else:
+            return False, row[2], row[0]
+    else:
+        return False, 0, 0
+
+
+def addUser(db, login, password, role):
+    cipher_key = b'aIgO-OFHtCwB6LgfBcl1IQPYTVjQHzNsyzHK_PICN3s='
+    cipher = Fernet(cipher_key)
+    encrypted_password = password.encode('utf8')
+    encrypted_password = cipher.encrypt(encrypted_password)
+    print(encrypted_password)
+    try:
+        db.cursor.execute("INSERT INTO Пользователи(Логин, Пароль, Роль) "
+                          "VALUES ('"+login+"','"+encrypted_password.decode("utf-8") +"',"+str(role)+")")
+        db.cnxn.commit()
+        print("ok")
+    except pyodbc.Error as e:
+        print("not ok")
+        print(e)
+
+def updatePassword(db, login, password):
+    cipher_key = b'aIgO-OFHtCwB6LgfBcl1IQPYTVjQHzNsyzHK_PICN3s='
+    cipher = Fernet(cipher_key)
+    encrypted_password = password.encode('utf8')
+    encrypted_password = cipher.encrypt(encrypted_password)
+    db.cursor.execute("UPDATE Пользователи SET Пароль='"+encrypted_password.decode("utf-8")+"' where Логин='"+login+"'")
+    db.cnxn.commit()
+
 
 if __name__ == '__main__':
     db = Sql("football_club")
-    db.cursor.execute("SELECT ФИО, Рост FROM Футболисты where Рост > 180")
-    row = db.cursor.fetchall()
-    print(row)
+
+    #db.cursor.execute("SELECT ФИО, Рост FROM Футболисты where Рост > 180")
+    #row = db.cursor.fetchall()
+    #print(row)
     app = QtWidgets.QApplication([])
     window = mainwindow()
     window.show()
+    db.cnxn.close()
     sys.exit(app.exec())
--- fcdb/registerFan.py
+++ fcdb/registerFan.py
@@ -9,7 +9,7 @@
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-import eye as eye_rc
+import eye
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
